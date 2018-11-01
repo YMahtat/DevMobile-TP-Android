335963b1a1d054654e50fc4bd04d2fdf645ca5b6
YMahtat <Youssef.MAHTAT@gmail.com>
2018-11-01 09:24:21 +0100

    X

---
+++ app/src/main/java/ma/emsi/part2/Profile.java
@@ -0,0 +1,14 @@
+package ma.emsi.part2;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Profile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+}
