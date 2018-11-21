cc4d87f6433544b80575c08832705ab8ac21654d
YMahtat <Youssef.MAHTAT@gmail.com>
2018-11-21 19:27:29 +0100

    First safe commit of TP

---
+++ app/src/main/java/ma/emsi/part2/Etudiant.java
@@ -0,0 +1,67 @@
+package ma.emsi.part2;
+
+import android.graphics.Bitmap;
+
+class Etudiant {
+    private String nom, prenom, classe, tel;
+    private Bitmap photo;
+    private int stdID;
+
+    Etudiant(int stdID, String nom, String prenom, Bitmap photo, String classe, String tel) {
+        this.stdID = stdID;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.photo = photo;
+        this.classe = classe;
+        this.tel = tel;
+    }
+
+
+    public void setStdID(int stdID) {
+        this.stdID = stdID;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public int getStdID() {
+        return stdID;
+    }
+
+    public Bitmap getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Bitmap photo) {
+        this.photo = photo;
+    }
+}
--- app/src/main/java/ma/emsi/part2/Profile.java
+++ app/src/main/java/ma/emsi/part2/Profile.java
@@ -1,9 +1,30 @@
 package ma.emsi.part2;
 
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Profile extends Activity {
+
+    private static final String DEBUGTAG = "PROFILE";
+    public static final String _urlWebService = "http://belatar.name/tests/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,4 +32,88 @@ public class Profile extends Activity {
         setContentView(R.layout.activity_profile);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new WSTask().execute(_urlWebService,"profile.php?login=test&passwd=test");
+    }
+
+    public void enregistrer(View view) {
+        Toast.makeText(this, getString(R.string.prfl_enrg), Toast.LENGTH_LONG)
+             .show();
+    }
+
+
+
+    public void test(View view){
+        Toast.makeText(this, "Sir tl3ab", Toast.LENGTH_LONG)
+                .show();
+    }
+
+    private class WSTask extends AsyncTask<String, Void, Etudiant>{
+
+        @Override
+        protected Etudiant doInBackground(String... strings) {
+
+            try {
+                URL url = new URL(strings[0] + strings[1]);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                InputStream in = con.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String resultat = "", ligne = "";
+                while ((ligne = reader.readLine()) != null)
+                    resultat += ligne + "\n";
+
+                Log.d(DEBUGTAG, resultat);
+                con.disconnect();
+                JSONObject json = new JSONObject(resultat);
+                if(json.has("error")) Log.e(DEBUGTAG, json.getString("error"));
+                else {
+
+                    URL urlPhoto = new URL( Profile._urlWebService + json.getString("photo"));
+                    HttpURLConnection conToGetPhoto = (HttpURLConnection) urlPhoto.openConnection();
+                    in = conToGetPhoto.getInputStream();
+
+
+                    Etudiant  etd =  new Etudiant(
+                                            json.getInt("id"),
+                                            json.getString("nom"),
+                                            json.getString("prenom"),
+                                            BitmapFactory.decodeStream(in),
+                                            json.getString("classe"),
+                                            json.getString("phone")
+                                        );
+                    conToGetPhoto.disconnect();
+                    return etd;
+                }
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Etudiant etudiant) {
+            super.onPostExecute(etudiant);
+            EditText inputNom = findViewById(R.id.inputNom);
+            EditText inputPrenom = findViewById(R.id.inputPrenom);
+            EditText inputClasse = findViewById(R.id.inputClasse);
+            ImageView imgProfil = findViewById(R.id.IMG_Profil);
+
+            inputNom.setText(etudiant.getNom());
+            inputPrenom.setText(etudiant.getPrenom());
+            inputClasse.setText(etudiant.getClasse());
+            imgProfil.setImageBitmap(etudiant.getPhoto());
+
+
+        }
+    }
 }
