54c4c7ad091132556cdf43a368bd6e6881b1f8d3
YMahtat <Youssef.MAHTAT@gmail.com>
2018-11-21 22:05:39 +0100

    Save Changes 21/11/2018 22:05

--- app/src/main/java/ma/emsi/tpAndroid/MainActivity.java
+++ app/src/main/java/ma/emsi/tpAndroid/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(ma.emsi.part2.R.layout.activity_main);
+        setContentView(ma.emsi.tpAndroid.R.layout.activity_main);
     }
 }
